022d8bba7d98e49ca9a24d5e88d619da87fd89ec
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-05 15:42:44 -0700

    Add default nil values to initialize parameters

--- node.rb
+++ node.rb
@@ -4,7 +4,7 @@
 class Node
   attr_reader :value, :next_node
 
-  def initialize(value, next_node)
+  def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end

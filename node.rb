792681f50b34dcb3f5d2b72849c929ab853de50b
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-05 15:32:28 -0700

    Develop Node class which holds data for value of the node and a pointer to next Node

--- node.rb
+++ node.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# class for Nodes contained within a LinkedList
 class Node
-  # placeholder
+  attr_reader :value, :next_node
+
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
+  end
 end

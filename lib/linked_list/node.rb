b8c21bd7c63fbca9d8a890056ab0a23db71d9df0
Josh Smith <jmsmith1018@gmail.com>
2025-11-29 09:14:00 -0800

    Reorganize lib

---
+++ lib/linked_list/node.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class LinkedList
+  class Node
+    attr_reader :value
+    attr_accessor :next_node
+
+    def initialize(value, next_node = nil)
+      @value = value
+      @next_node = next_node
+    end
+
+    def to_s = value.to_s
+  end
+end
